28f8582517d976ac67a4dee492eae5a4bf7e26a3
Elan Fraga <elanfraga@gmai.com>
2020-04-23 19:58:15 -0300

    Página de cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import GlobalStyle from './styles/global';
-import Signin from './pages/Signin';
+// import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 const App: React.FC = () => (
   <>
-    <Signin />
+    <SignUp />
     <GlobalStyle />
   </>
 );
