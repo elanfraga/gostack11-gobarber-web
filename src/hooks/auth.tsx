c9458de2d1e135efe93d44cb62db349e21b8031b
Elan Fraga <elanfraga@gmail.com>
2020-05-26 08:06:03 -0300

    Disponibilidade do mês

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -31,8 +31,11 @@ const AuthProvider: React.FC = ({ children }) => {
     const user = localStorage.getItem('@Gobarber:user');
 
     if (token && user) {
+      api.defaults.headers.authorization = `Bearer ${token}`;
+
       return { token, user: JSON.parse(user) };
     }
+
     return {} as AuthState;
   });
 
@@ -46,6 +49,8 @@ const AuthProvider: React.FC = ({ children }) => {
 
     localStorage.setItem('@GoBarber:token', token);
     localStorage.setItem('@Gobarber:user', JSON.stringify(user));
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
